cf8ec7aa8ca14186273cda9cf9e48d9a8416163b
Bijay Jha <bijayjha12345@gmail.com>
2026-07-13 10:35:43 +0545

    basic arithmetic in scalar and vector

---
+++ NumPy/arithmetic.py
@@ -0,0 +1,23 @@
+
+#Scalar arithmetic
+
+import numpy as np
+
+array = np.array([1.56, 2.45, 3.78])
+print(array + 1) #This will add 1 to each element of the array.
+print(array - 1) #This will subtract 1 from each element of the array.
+print(array * 2) #This will multiply each element of the array by 2.
+print(array / 4) #This will divide each element of the array by 4.
+print(array ** 5) #This will raise each element of the array to the power of 5.
+print(array % 2) #This will return the remainder of each element of the array when divided by 2.
+
+
+#Vectorized math functions
+print(np.sqrt(array)) #This will return the square root of each element of the array.
+print(np.exp(array)) #This will return the exponential of each element of the array.
+print(np.round(array)) #This will round each element of the array to the nearest integer.
+print(np.floor(array)) #This will return the largest integer less than or equal to each element of the array.
+print(np.ceil(array)) #This will return the smallest integer greater than or equal to each element of the array.
+print(np.pi) #This will return the value of pi.
+print(np.sin(array)) #This will return the sine of each element of the array.   
+print(np.cos(array)) #This will return the cosine of each element of the array.
