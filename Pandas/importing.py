9674c5ba83e4061d8de999d19aa5854a761da973
Bijay Jha <bijayjha12345@gmail.com>
2026-07-22 20:48:23 +0545

    inserting csv and json file

--- Pandas/importing.py
+++ Pandas/importing.py
@@ -1,11 +1,16 @@
 #In this, we will learn how to work with importing data in pandas. Importing data is a crucial step in data analysis, as it allows us to bring external data into our Python environment for further processing and analysis. Pandas provides various functions to import data from different file formats, such as CSV, Excel, SQL databases, and more.
 
-import pandas as pd #To import data from a CSV file, we can use the read_csv() function provided by pandas. This function takes the file path of the CSV file as an argument and returns a DataFrame object containing the data from the CSV file. We can also specify additional parameters to customize the importing process, such as specifying the delimiter, handling missing values, and selecting specific columns.
+#To import data from a CSV file, we can use the read_csv() function provided by pandas. This function takes the file path of the CSV file as an argument and returns a DataFrame object containing the data from the CSV file. We can also specify additional parameters to customize the importing process, such as specifying the delimiter, handling missing values, and selecting specific columns.
 
-#For example, to import data from a CSV file named "data.csv", we can use the following code:
 
-data = pd.read_csv("data.csv") #Here, read_csv("data.csv") is a function that reads the CSV file located at the specified file path and returns a DataFrame object containing the data from the CSV file. The resulting DataFrame will have columns corresponding to the headers in the CSV file and rows corresponding to the data entries.
+import pandas as pd
+df = pd.read_csv(r"C:\Users\pc\Desktop\New\Pandas\Data\data.csv")
+#Here, read_csv("data.csv") is a function that reads the CSV file located at the specified file path and returns a DataFrame object containing the data from the CSV file. The resulting DataFrame will have columns corresponding to the headers in the CSV file and rows corresponding to the data entries.
 
-print(data) #This will print the DataFrame object, displaying the column labels and the corresponding data from the CSV file. The DataFrame will have as many columns as there are headers in the CSV file and as many rows as there are data entries.
+print(df)
 
-#will later add data.csv file to the repo so that you can run the code and see the output.
+
+df1 = pd.read_json(r"C:\Users\pc\Desktop\New\Pandas\Data\data.json", orient="records")
+print(df1)
+
+#Here, orient = records specifies that the JSON data is in a list of records format, where each record is represented as a dictionary. The resulting DataFrame will have columns corresponding to the keys in the dictionaries and rows corresponding to the individual records.
